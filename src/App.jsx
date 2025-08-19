3e43bb7c6856c5413894324702baf60f545e53fc
liightyz <lightz0217@gmail.com>
2025-08-19 08:04:14 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Xuxa1 from './assets/notebook-xuxa.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,16 @@ function App() {
       <main>
         <header></header>
         <section className="produtos">
+
+          {/* Produto 1 */}
           <div className="card">
-            <img src="" alt="" />
-            <h2></h2>
-            <p></p>
-            <p className="avaliacao"></p>
+            <img src={Xuxa1} alt="" className='img-card'/>
+            <h2 className="titulo-card">Notebook Xuxa</h2>
+            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
+            <p className="preco">R$ 5.999,99</p>
+            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
           </div>
+
         </section>
         <section className="destaque"></section>
         <footer></footer>
